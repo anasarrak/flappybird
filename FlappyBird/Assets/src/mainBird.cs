2cbe7d2470082e98153a3add6c3dbef840984275
Unknown <anas.mchichou@ikasle.egibide.org>
2018-04-12 21:51:12 +0200

    Añadir bird y ground
    Añadir bird y ground

---
+++ FlappyBird/Assets/src/mainBird.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mainBird : MonoBehaviour {
+public float Speed = 5.0f;
+public bool msg = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonDown(0)){
+            jump();
+		}
+
+    	if(msg){
+    		//alert("toca");
+    	}
+        
+
+		
+	}
+	void jump(){
+        GetComponent<Rigidbody2D>().velocity = Vector3.up * Speed;	
+	}
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.name == "ground"){
+			msg = true;
+			print("toca");
+		}
+	}
+
+}
+
+
